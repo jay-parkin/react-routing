a1d0a4567b167d7e49e356dfe7b117167802f73c
Jason Parkin <jay-parkin91@hotmail.com>
2024-09-21 12:05:08 +1000

    feat: link to nested page

--- src/components/App_ComponentRouter.jsx
+++ src/components/App_ComponentRouter.jsx
@@ -3,7 +3,9 @@ import "../styles/App.css";
 import HomePage from "../pages/HomePage";
 import AboutPage from "../pages/AboutPage";
 import ContactPage from "../pages/ContactPage";
+import ProjectLayout from "../pages/_templates/ProjectLayout";
 import Navbar from "./NavBar";
+import PokemonProject from "../pages/projects/PokemonProject";
 
 function App() {
   return (
@@ -16,6 +18,9 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/contact" element={<ContactPage />} />
+          <Route path="/projects" element={<ProjectLayout />}>
+            <Route path="pokemon" element={<PokemonProject />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
